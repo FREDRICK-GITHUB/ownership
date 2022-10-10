087098aaebcacad3cca69d9f2a8dd793da5b80e0
FREDRICK-GITHUB <kamaufredm@gmail.com>
2022-10-10 09:48:24 +0300

    show how to take and give back ownership of a variable

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,18 @@
 fn main() {
-    let s = String::from("hello");
-    takes_ownership(s);
+    let s1 = gives_ownership();
 
+    let s2 = String::from("hello");
 
-    let x = 5;
-    makes_copy(x);
-    }
-
-
-fn takes_ownership(somestring: String){
-    println!("{}", somestring);
+    let s3 = gives_and_takes_back(s2);
+    println!("the value of s1 is: {} and s3 is: {}", s1, s3);
+  
 }
 
-fn makes_copy(some_integer: i32){
-    println!("{}", some_integer);
+fn gives_ownership() -> String {
+    let some_string = String::from("hello");
+    some_string
+}
+
+fn gives_and_takes_back(a_string: String) -> String {
+    a_string
 }
