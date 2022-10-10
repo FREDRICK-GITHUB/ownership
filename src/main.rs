a976f5322fefc1b3a6e410dc518fa0d5907a7c53
FREDRICK-GITHUB <kamaufredm@gmail.com>
2022-10-10 10:09:16 +0300

    references example showing borrowing concept

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
 fn main() {
-    let s1 = gives_ownership();
+    let s1 = String::from("hello");
+  
+    let len = calculate_length(&s1);
 
-    let s2 = String::from("hello");
+    println!("the length of {} is: {}", s1, len);
 
-    let s3 = gives_and_takes_back(s2);
-    println!("the value of s1 is: {} and s3 is: {}", s1, s3);
   
 }
 
-fn gives_ownership() -> String {
-    let some_string = String::from("hello");
-    some_string
+fn calculate_length(s: &String) -> usize {
+    s.len()
 }
 
-fn gives_and_takes_back(a_string: String) -> String {
-    a_string
-}
+
+
+
+
